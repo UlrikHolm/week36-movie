a91f5202a84437c55e9b3dfd9238e8c1b210fb25
Ulrik Holm <ulrikholmuh@gmail.com>
2019-09-03 23:53:28 +0200

    first commit

---
+++ src/main/java/entities/Movie.java
@@ -0,0 +1,61 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+
+@Entity
+@NamedQuery(name = "Movie.deleteAllRows", query = "DELETE from Movie")
+public class Movie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String director;
+    private int year;
+    
+    public Movie() {
+    }
+
+    public Movie(String title, String director, int year) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+    }
+        
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+    // TODO, delete this class, or rename to an Entity class that makes sense for what you are about to do
+    // Delete EVERYTHING below if you decide to use this class, it's dummy data used for the initial demo
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+       
+}
